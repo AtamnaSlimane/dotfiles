600e372aa92732bb31bdfd9a37a965205230341e
AtamnaSlimane <atamnamohamedslimane@gmail.com>
2025-11-01 12:41:43 +0100

    dartls

--- nvim/lua/configs/lspconfig.lua
+++ nvim/lua/configs/lspconfig.lua
@@ -1,6 +1,6 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls","clangd","pyright","jdtls","phpactor","intelephense","gopls","css-lsp" }
+local servers = { "html", "cssls","clangd","pyright","jdtls","phpactor","intelephense","gopls","css-lsp","dartls" }
 vim.lsp.enable(servers)
 
 -- read :h vim.lsp.config for changing options of lsp servers 
