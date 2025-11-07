cebb8240adaff10bee795caf4b83d42f02f7a979
AtamnaSlimane <atamnamohamedslimane@gmail.com>
2025-11-07 16:03:43 +0100

    added jj

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -5,7 +5,17 @@ require "nvchad.mappings"
 local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
-map("i", "jk", "<ESC>")
+map("i", "jj", "<ESC>")
 
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.hl.on_yank()
+  end,
+})
+
+vim.opt.relativenumber=true
 -- inside terminal mode
-vim.keymap.set('t', '<Esc>', [[<C-\><C-n>]], { desc = "Exit terminal insert mode" })
+-- when  pressing jj it entern nteminal which can be exited with space x
+vim.keymap.set('t', 'jjj', [[<C-\><C-n>]], { desc = "Exit terminal insert mode" })
